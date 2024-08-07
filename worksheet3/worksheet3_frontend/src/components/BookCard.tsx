15ce91eacc0234592239679466de7119ec4904c5
jamesduong03 <135298206+jamesduong03@users.noreply.github.com>
2024-08-08 09:30:22 +1200

    Adding worksheet 3 to repo

---
+++ worksheet3/worksheet3_frontend/src/components/BookCard.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "next/navigation";
+import { Book } from "./Book";
+
+interface IProp {
+    book?: Book;
+}
+
+const BookCard = ({ book }: IProp) => {
+    const router = useRouter();
+    if (book == undefined) {
+        return null;
+    }
+    const onClick = () => {
+        router.push(`/show-book/${book._id}`)
+    };
+    return (
+        <div className="card-container" onClick={onClick}>
+            <img
+                src="https://images.unsplash.com/photo-1495446815901-a7297e633e8d"
+                alt='Books'
+                height={200}
+            />
+            <div className="desc">
+                <h2>
+                    {book.title}
+                </h2>
+                <h3>
+                    {book.author}
+                </h3>
+                <p>
+                    {book.description}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
